5007dfae6a0af1368855312b68e23626dd022c98
reversed_coffee <realnickk1@gmail.com>
2024-06-02 23:34:09 -0500

    Set up HTTP/S

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+    site: "https://www.keyflow.systems",
     publicDir: "./static",
-    outDir: "./www"
+    outDir: "./www",
+    vite: {
+        server: {
+            host: "0.0.0.0",
+            port: 8443,
+            https: {
+                cert: "./certs/cert.crt",
+                key: "./certs/cert.key"
+            }
+        }
+    }
 });
