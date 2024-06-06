4a8fbb87f27c63a80d54e869e34ce782c3a48162
reversed_coffee <realnickk1@gmail.com>
2024-06-06 03:08:07 -0500

    Push website
    Co-authored-by: ayden <932554@users.noreply.github.com>

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Hero from "./Hero.astro";
+import Button from "./Button.astro";
+import Footer from "./Footer.astro";
+import Header from "./Header.astro";
+
+export { Hero, Button, Footer, Header };
